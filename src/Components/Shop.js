0c9d547e7a91747e9b568894189209e8139bc530
Rana Samir <34241329+samirrana1011@users.noreply.github.com>
2021-12-17 14:11:34 +0530

    Redux in react initial commit
    Redux in react initial commit

---
+++ src/Components/Shop.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Shop() {
+    return (
+        <div>
+            <h2>Buy nike shoes 50 RS</h2>
+            <button className="btn btn-primary mx-2">
+                -
+            </button>
+            Add thi item to cart
+            <button className="btn btn-primary mx-2">
+                +
+            </button>
+
+        </div>
+
+    )
+}
+
+export default Shop
